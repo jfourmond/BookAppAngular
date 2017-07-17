f285c11edc7aaa2b97e1edc0c85f51ef4e2b61f8
jfourmond <jerome@fourmond.fr>
2017-07-17 17:04:51 +0200

    test

--- js/config.js
+++ js/config.js
@@ -1,62 +1,5 @@
 'use strict';
 
-/*	CLASSE	*/
-class Book {
-	constructor(isbn, title, author, publisher, publishedDate, description, pageCount, thumbnail, favorite, addedDate) {
-		this.isbn = isbn;
-		this.title = title;
-		this.author = author;
-		this.publisher = publisher;
-		this.publishedDate = publishedDate;
-		this.description = description;
-		this.pageCount = pageCount;
-		this.thumbnail = thumbnail;
-		this.favorite = favorite;
-		this.addedDate = addedDate;
-	}
-}
-
-/*	TRAITEMENT DES DONNEES	*/
-function treatmentItems(items) {
-	var books = [];
-	for(var item of items)
-		books.push(treatmentItem(item));
-	return books;
-}
-
-function treatmentItem(item) {
-	// console.log(items);
-	var volume = item.volumeInfo;
-	var isbn = treatmentISBN13(volume.industryIdentifiers);
-	var title = volume.title;
-	var author = treatmentAuthors(volume.authors);
-	var publisher = volume.publisher;
-	var publishedDate = volume.publishedDate;
-	var description = volume.description;
-	var pageCount = volume.pageCount;
-	var thumbnail = null;
-	if(volume.imageLinks)
-	 	thumbnail = volume.imageLinks.thumbnail;
-	var favorite = false;
-	var addedDate = new Date();
-	return new Book(isbn, title, author, publisher, publishedDate, description, pageCount, thumbnail, favorite, addedDate);
-}
-
-function treatmentISBN13(industryIdentifiers) {
-	var isbn;
-	for(var id of industryIdentifiers)
-		if(id.type == "ISBN_13")
-			isbn = id.identifier;
-	return isbn;
-}
-
-function treatmentAuthors(authors) {
-	var author = authors[0];
-	for(var i=1; i < authors.size ; i++)
-		author = author + ", " + authors[i];
-	return author;
-}
-
 /*	APP	*/
 var bookApp = angular.module("BookApp", ["ngRoute"]);
 // var routeAppControllers = angular.module("routeAppControllers", []);
@@ -223,4 +166,38 @@ bookApp.controller('LibraryCtrl', function ($scope, $http) {
 		downloadLink.setAttribute('href', window.URL.createObjectURL(blob));
 		downloadLink.click();
 	};
+});
+
+bookApp.controller('SearchCtrl', function ($scope, $http) {
+	/*	PROGRESS BAR	*/
+	var spinner = document.querySelector("#spinner");
+	// INITIALISATION
+	$scope.books = [ ];
+	$scope.search = function() {
+		if($scope.isbn) {
+			var isbn = $scope.isbn;
+			var isbn = isbn.replace('-', '');	// Suppression du caractère obsolète '-'
+			console.log("Recherche de l'ISBN : " + isbn);
+			spinner.hidden = false;
+			$http.get("https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn)
+				.then(function success(response) {
+					var data = response.data;
+					console.log("Réponse de l'API : %O", response.data);
+					if(data.totalItems != 0) {
+						$scope.books = treatmentItems(data.items);
+						console.log("Books : %O", $scope.books);
+					} else {
+						$scope.books = [ ];
+						console.log("Aucun livre ne correspond à l'isbn " +  isbn);
+					}
+					spinner.hidden = true;
+				}, function failure(response) {
+					console.log("ECHEC");
+					spinner.hidden = true;
+				});
+		}
+	}
+	$scope.addBook = function(book) {
+		console.log("Ajout du livre : %O", book);
+	}
 });
---
+++ js/script.js
@@ -0,0 +1,56 @@
+/*	CLASSE	*/
+class Book {
+	constructor(isbn, title, author, publisher, publishedDate, description, pageCount, thumbnail, favorite, addedDate) {
+		this.isbn = isbn;
+		this.title = title;
+		this.author = author;
+		this.publisher = publisher;
+		this.publishedDate = publishedDate;
+		this.description = description;
+		this.pageCount = pageCount;
+		this.thumbnail = thumbnail;
+		this.favorite = favorite;
+		this.addedDate = addedDate;
+	}
+}
+
+/*	TRAITEMENT DES DONNEES	*/
+function treatmentItems(items) {
+	var books = [];
+	for(var item of items)
+		books.push(treatmentItem(item));
+	return books;
+}
+
+function treatmentItem(item) {
+	// console.log(items);
+	var volume = item.volumeInfo;
+	var isbn = treatmentISBN13(volume.industryIdentifiers);
+	var title = volume.title;
+	var author = treatmentAuthors(volume.authors);
+	var publisher = volume.publisher;
+	var publishedDate = volume.publishedDate;
+	var description = volume.description;
+	var pageCount = volume.pageCount;
+	var thumbnail = null;
+	if(volume.imageLinks)
+	 	thumbnail = volume.imageLinks.thumbnail;
+	var favorite = false;
+	var addedDate = new Date();
+	return new Book(isbn, title, author, publisher, publishedDate, description, pageCount, thumbnail, favorite, addedDate);
+}
+
+function treatmentISBN13(industryIdentifiers) {
+	var isbn;
+	for(var id of industryIdentifiers)
+		if(id.type == "ISBN_13")
+			isbn = id.identifier;
+	return isbn;
+}
+
+function treatmentAuthors(authors) {
+	var author = authors[0];
+	for(var i=1; i < authors.size ; i++)
+		author = author + ", " + authors[i];
+	return author;
+}
